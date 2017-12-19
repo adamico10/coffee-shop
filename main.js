4d27e4bb7decce48fb7b3a11bcbbb9c115b552e8
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2017-12-19 13:18:48 +0200

    index title update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+
 var coffeeShop = {
   beans: 40,
   money: 0,
   beanscost: 2,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {beanRequirement: 10, price: 50} ,
+    americano: {beanRequirement: 5, price: 22}, 
+    doubleShot: {beanRequirement: 15, price: 100},
+    frenchPress: {beanRequirement: 12, price: 77}
   },
-  
   buySupplies: function(purchBeans) {
     if(purchBeans > 0) {
       coffeeShop.money = this.money - (purchBeans * beanscost)
@@ -17,15 +17,18 @@ var coffeeShop = {
   },
 
   buyDrink: function(drinkType) {
-    if (drinkType == coffeeShop.drinkRequirements.hasOwnProperty(drinkType)) {
-      makeDrink(drinkType) 
+    if (this.drinkRequirements.hasOwnProperty(drinkType)) {
+      this.makeDrink(drinkType) 
+      this.money = this.money + this.drinkRequirements[drinkType].price;
     }
   },
-  
+
   makeDrink: function (drinkType) {
-    if (coffeeShop.drinkRequirements.hasOwnProperty(drinkType) && (coffeeShop.beans > coffeeShop.drinkRequirements[drinkType])) {
-      coffeeShop.beans = this.beans - coffeeShop.drinkRequirements[drinkType];
-    } else if (coffeeShop.beans < coffeeShop.drinkRequirements[drinkType]){
+    if (this.drinkRequirements.hasOwnProperty(drinkType) &&
+     (this.beans > this.drinkRequirements[drinkType].beanRequirement)) {
+      this.beans = this.beans - this.drinkRequirements[drinkType].beanRequirement;
+    } else if (this.drinkRequirements.hasOwnProperty(drinkType) && 
+    this.beans < this.drinkRequirements[drinkType].beanRequirement) {
        alert("Sorry, we're all out of beans!")
     } else {
       alert("Sorry, we don't make " + drinkType)
@@ -33,8 +36,18 @@ var coffeeShop = {
   }
 }
 
+
 coffeeShop.makeDrink("latte");
 coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+
+// buy drinks
+/*
+coffeeShop.buyDrink("latte");
+coffeeShop.buyDrink("americano");
+coffeeShop.buyDrink("filtered");
+coffeeShop.buyDrink("doubleShot");
+coffeeShop.buyDrink("frenchPress");
+*/
